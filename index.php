d87b482f16c5a254af13d25d5ab102452d6a367f
vk-work <vkywn1992@gmail.com>
2021-01-23 23:41:52 +0500

    First Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+  	<title>Globe</title>
+  	<style type="text/css">
+  		body {
+  			margin: 0px;
+  			padding: 0px;
+  		}
+  		canvas {
+  			background-color: #000;
+  		}
+  	</style>
+  </head>
+  <body>
+    <div id="globe" style="height: 100vh">
+      <canvas style="height: 100%;"></canvas>
+    </div>
+
+    <script src="./three.js"></script>
+    <script src="./orbital-controls.js"></script>
+    <script src="./globe.js"></script>
+  </body>
+</html>
